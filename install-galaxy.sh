47ab30d6b9df7c302568fc6e4f041d3dd6075a63
mario <mariocuaya5@gmail.com>
2024-07-29 21:50:40 -0700

    Modifications for HPCC use

--- install-galaxy.sh
+++ install-galaxy.sh
@@ -69,6 +69,17 @@ run_common_start_up
 
 setup_python
 
+#Releated to Galaxy 24.0
+#if [ ! -z "$GALAXY_RUN_WITH_TEST_TOOLS" ];
+#then
+#    export GALAXY_CONFIG_OVERRIDE_TOOL_CONFIG_FILE="$(pwd)/test/functional/tools/sample_tool_conf.xml"
+#    export GALAXY_CONFIG_ENABLE_BETA_WORKFLOW_MODULES="true"
+#    export GALAXY_CONFIG_OVERRIDE_ENABLE_BETA_TOOL_FORMATS="true"
+#    export GALAXY_CONFIG_INTERACTIVETOOLS_ENABLE="true"
+#    export GALAXY_CONFIG_OVERRIDE_WEBHOOKS_DIR="test/functional/webhooks"
+#    export GALAXY_CONFIG_OVERRIDE_PANEL_VIEWS_DIR="$(pwd)/test/integration/panel_views_1/"
+#fi
+
 if [ ! -z "$GALAXY_RUN_WITH_TEST_TOOLS" ];
 then
     export GALAXY_CONFIG_OVERRIDE_TOOL_CONFIG_FILE="test/functional/tools/samples_tool_conf.xml"
@@ -87,3 +98,5 @@ if [ "$INITIALIZE_TOOL_DEPENDENCIES" -eq 1 ]; then
     # Install Conda environment if needed.
     python ./scripts/manage_tool_dependencies.py init_if_needed
 fi
+
+
