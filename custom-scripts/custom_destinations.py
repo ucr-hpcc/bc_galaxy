8a923300dd35ac1d99bcc49b23321f7d007841c3
Mario Cuaya <mcuay001@ucr.edu>
2025-05-02 16:40:15 -0700

    Finalizing module system implementation within galaxy

--- custom-scripts/custom_destinations.py
+++ custom-scripts/custom_destinations.py
@@ -1,27 +1,63 @@
 import logging
-from galaxy.jobs.mapper import JobMappingException
-from galaxy.jobs import JobDestination
 
 log = logging.getLogger(__name__)
 
-FAILURE_MESSAGE = 'This tool could not be run because of a misconfiguration in the Galaxy job running system, please report this error'
+FAILURE_MESSAGE = 'This tool could not be run because of a misconfiguration in the Galaxy job running system, please report this error to support@hpcc.ucr.edu'
 
 
-def dynamic_cores_time(app, tool, job, user_email, resource_params):
-    # handle job resource parameters
-    if not resource_params.get("cores") and not resource_params.get("time"):
-        default_destination_id = app.job_config.get_destination(None)
-        log.warning('(%s) has no input parameter cores or time. Run with default runner: %s' % (job.id, default_destination_id.runner))
-        return default_destination_id
-    
+def dynamic_cores_time(app,
+                       tool,
+                       job,
+                       user):
     try:
-        cores = resource_params.get("cores")
-        time = resource_params.get("time")
-        resource_list = 'walltime=%s:00:00,nodes=1:ppn=%s' % (time, cores)
-    except:
-        default_destination_id = app.job_config.get_destination(None)
-        log.warning('(%s) failed to run with customized configuration. Run with default runner: %s' % (job.id, default_destination_id.runner))
-        return default_destination_id
+        # Set default runner and parse the selected runner
+        default_runner = app.job_config.get_destination('local')
+        selected_runner = app.job_config.dynamic_params['runner']
 
-    log.info('returning pbs runner with configuration %s', resource_list)
-    return JobDestination(runner="pbs", params={"Resource_List": resource_list})
+
+        # This holds the necessary values to pass in!!!
+        # Check if user is running custom module wrapper tool. The tool should have a parameter called "selected_runner"
+        if tool.get_param("selected_runner") != None:
+            params = job.get_param_values(app)
+
+            # Get selected_runner from params dictonary
+            runner = params['selected_runner']
+
+            # Check if job runner is slurm
+            if runner['runner'] == "slurm":
+
+                # Extract selected values
+                cores = runner['core']
+                mem = runner['memory']
+                runtime = runner['runtime']
+                partition_selected = runner['partition']
+
+                log.info('Returning slurm runner with defined configurations set...')
+
+                # Create the actual job runner object for slurm
+                slurm_runner = app.job_config.get_destination('slurm')
+
+                # Check if walltime was defined
+                if runtime != None:
+                    slurm_runner.params['nativeSpecification'] = f"--cpus-per-task={cores} --mem={mem} --partition={partition_selected} --time={runtime} --job_name={user.username}/{tool.id}"
+                else:
+                    slurm_runner.params['nativeSpecification'] = f"--cpus-per-task={cores} --mem={mem} --partition={partition_selected} --job_name={user.username}/{tool.id}"
+
+                return slurm_runner
+
+        # If user is not running custom module wrapper tool, check if the selected runner is slurm
+        elif selected_runner == 'slurm':
+            log.info('Returning slurm runner with default configurations set...')
+            slurm_runner = app.job_config.get_destination('slurm')
+
+            # Return slurm with default parameters
+            slurm_runner.params['nativeSpecification'] = f"--cpus-per-task=8 --mem=16000 --job_name={user.username}/{tool.id}"
+            return slurm_runner
+
+        log.info('Returning local runner...')
+        return default_runner
+    except Exception as error:
+        log.info(FAILURE_MESSAGE)
+        log.error(error)
+        return None
+
--- custom-scripts/custom_tool_form_utils.py
+++ custom-scripts/custom_tool_form_utils.py
@@ -7,31 +7,49 @@ from typing import(
 )
 
 log = logging.getLogger(__name__)
+FAILURE_MESSAGE = 'This tool could not be run because of a misconfiguration in the Galaxy job running system, please report this error to support@hpcc.ucr.edu'
+
+# Function which custom module tool wrappers call to determine which runner to use for job
 def determine_runner() -> List[Tuple[str, str, bool]]:
     try:
+        # Check value of enviornment variable set during OnDemand start up phase
         if os.environ.get("RUNNER") == "slurm":
             log.info('Slurm avaible, selecting slurm as primary job runner for configured tool...')
             return [("Yes", "slurm", True)]
+        # Return selected runner in the following format
         return [("No", "local", True)]
     except Exception as error:
-        log.info(error)
+        log.error(error)
+        log.error(FAILURE_MESSAGE)
         return [("Error", "error", True)]
 
+# Function which displays all partitions available to the user
 def partitions_available() -> List[Tuple[str, str, bool]]:
     try:
+        # Lanuch a process with the following command and capture it's output as text
         get_partition = subprocess.run(["sinfo", "-O", "partitionname"], capture_output=True, text=True)
+
+        # Split output by newlines
         get_partition = str(get_partition.stdout).split('\n')
+
+        # First value with have unnecessary information
         get_partition.pop(0)
-        get_partition.pop(len(get_partition)-1)
+
+        # The last value is just an empty line so pop it
+        get_partition.pop()
         partitions = []
+
+        # Store partitions in a triple format ('label', 'value', selected or not: True or False)
         for i in range(len(get_partition)):
             name = get_partition[i].strip()
+            # Set hpcc_default as the default partition
             if name == "hpcc_default":
                 partitions.append((name, name, True))
             else:
                 partitions.append((name, name, False))
         return partitions
     except Exception as error:
-        log.info(error)
+        log.error(error)
+        log.error(FAILURE_MESSAGE)
         return [("Error", "error", True)]
 
