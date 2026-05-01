53e95d2bc95b2dc09f37e2608f09dc93ac6345da
Mario Cuaya <mcuay001@ucr.edu>
2026-05-01 10:07:08 -0700

    Updates to bootstrap script

--- install-galaxy-part2.sh
+++ install-galaxy-part2.sh
@@ -9,18 +9,24 @@ cd $VERSION
 # Check if bootstrap directory exists
 if [[ ! -d "$PWD/bootstrap" ]]; then
 	echo "Creating bootstrap directory..."
-	mkdir -p "bootstrap/shed_tools_data" "bootstrap/config" "bootstrap/dependencies/_conda"
+	mkdir -p "bootstrap/shed_tools_data" \
+		 "bootstrap/shed_tools_data_manager" \
+		 "bootstrap/config" \
+		 "bootstrap/dependencies/_conda"
 	echo "Cloning usegalaxy-tools repo"
 	git clone https://github.com/galaxyproject/usegalaxy-tools.git
 	echo "Installing requirements.txt..."
 	$PWD/.venv/bin/python -m pip install -r usegalaxy-tools/requirements.txt
 	mv usegalaxy-tools bootstrap/
 	touch $PWD/bootstrap/install_tool_sheds.sqlite
+else
+	echo "Delete or move old bootstrap directory and rerun the script!"
+	exit 1
 fi
 
 # Export necessary environment variables
 export GALAXY_CONFIG_FILE="$PWD/bootstrap/config/galaxy.yml"
-export GALAXY_ROOT_DIR="$PWD/"
+export GALAXY_ROOT_DIR="$PWD"
 
 # Generate bootstrap api key
 RANDOM_KEY="$(openssl rand -base64 8)"
@@ -44,8 +50,8 @@ galaxy:
   install_database_connection: sqlite:///$PWD/bootstrap/install_tool_sheds.sqlite
   conda_auto_init: true
   conda_prefix: $PWD/bootstrap/dependencies/_conda
-  log_destination: $PWD/bootstrap/galaxy-bootstrap.log
   shed_tool_data_path: $PWD/bootstrap/shed_tools_data
+  galaxy_data_manager_data_path: $PWD/bootstrap/shed_tools_data_manager
   shed_tool_config_file: $PWD/bootstrap/config/bootstrap_tools_conf.xml
 EOL
 )
@@ -57,24 +63,31 @@ python $GALAXY_ROOT_DIR/scripts/db.py -c ${GALAXY_CONFIG_FILE} init
 
 
 echo "Launching Galaxy in daemon mode..."
-# Run galaxy in background
-sh $GALAXY_ROOT_DIR/run.sh --daemon --no-create-venv --skip-client-build --skip-wheels
+# Run galaxy in background, without using --daemon
+nohup setsid sh $GALAXY_ROOT_DIR/run.sh --no-create-venv --skip-client-build --skip-wheels > bootstrap.log 2>&1 &
+GALAXY_PID=$!
+echo "Galaxy PID: ${GALAXY_PID}"
 
 # Wait until galaxy is accessible
-galaxy-wait -g http://localhost:8080 -v --timeout 180
+if ! galaxy-wait -g http://localhost:8080 -v --timeout 180; then
+	echo "Unable to access Galaxy server..."
+	kill -INT -$(ps -o pgid= -p $GALAXY_PID)
+	exit 1
+fi
 
-echo "Installing tools from usegalaxy-tools repo. This may take awhile..."
+shed-tools install --tools-file $PWD/usegalaxy-tools/usegalaxy.org/nanopore.yml.lock --latest --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
+#TOOLS_TO_INSTALL=( $(ls $PWD/usegalaxy-tools/usegalaxy.org/*.yml.lock) )
+#for tool in "${TOOLS_TO_INSTALL[@]}"; do
+#	echo "Installing: ${tool}"
+#	shed-tools install --tools-file ${tool} --latest --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
+#done
 
-TOOLS_TO_INSTALL=( $(ls $PWD/usegalaxy-tools/usegalaxy.org/*.yml.lock) )
-for tool in "${TOOLS_TO_INSTALL[@]}"; do
-	echo "Installing: ${tool}"
-	shed-tools install --tools-file ${tool} --install-tool-dependencies --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
-done
+# Install tool dependencies. This installs any missing tool dependencies via conda
+install-tool-deps --tool $PWD/config/bootstrap_tools_conf.xml --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
 
-# Install tool dependencies. This installs any missing tools via conda
-install-tool-deps --tool $PWD/config/shed_tool_conf.xml --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
-
-sh $GALAXY_ROOT_DIR/run.sh --stop-daemon --no-create-venv --skip-client-build --skip-wheels
+echo "Attempting to shutdown the Galaxy server..."
+kill -INT -$(ps -o pgid= -p $GALAXY_PID)
+echo "Galaxy server shutdown!"
 
 cd ..
 
@@ -92,4 +105,6 @@ rm -rf bootstrap/cache \
        bootstrap/results.sqlite \
        bootstrap/config/galaxy.yml
 
-echo "Finished! Check galaxy-bootstrap.log in bootstrap/ for any errors..."
+echo "Finished! Check bootstrap.log in bootstrap/ for any errors..."
+
+exit 0
