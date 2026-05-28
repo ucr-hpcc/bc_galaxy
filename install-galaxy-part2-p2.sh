95dde8ef8e4fe3d00d3c67ce49ef1f4ffb4c282c
Mario Cuaya <mcuay001@ucr.edu>
2026-05-28 12:54:00 -0700

    Update to part2-p2 of install galaxy script

--- install-galaxy-part2-p2.sh
+++ install-galaxy-part2-p2.sh
@@ -33,3 +33,22 @@ done
 install-tool-deps --tool $PWD/config/bootstrap_tools_conf.xml --verbose --galaxy 'http://localhost:8080' --api-key ${RANDOM_KEY}
 
 echo "Bootstrap tools initialized! Please shut down the Galaxy server..."
+echo "Once the server is shutdown, remove the following directories/files:
+
+       bootstrap/cache
+       bootstrap/container_cache
+       bootstrap/control.sqlite
+       bootstrap/gravity
+       bootstrap/object_store_cache
+       bootstrap/tmp
+       bootstrap/config/galaxy.yml
+       bootstrap/tool_search_index
+       bootstrap/universe.sqlite
+       bootstrap/results.sqlite
+       bootstrap/config/galaxy.yml
+
+After the listed directories/files are removed, update the permissions of the bootstrap/config directory to make it readable for all users:
+
+	chmod -R a+r bootstrap/config
+
+Once all these changes are made, Galaxy is ready for public use!!!"
